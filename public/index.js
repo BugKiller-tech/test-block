ce3420c7766e539d0dca1f06845a63a463c9b63b
Joshua Owen <jowen94o@gmail.com>
2018-04-09 22:47:13 -0500

    added firebase database integration

---
+++ public/index.js
@@ -0,0 +1,18 @@
+var database = firebase.database();
+
+
+function writeUserData() {
+  let pageName = document.getElementById("username").value;
+  let pageCompanyName = document.getElementById("companyName").value;
+  let pageEmail = document.getElementById("email").value;
+  let pageJobPosition = document.getElementById("jobPosition").value;
+  let pageJobDescription = document.getElementById("jobDescription").value;
+
+  firebase.database().ref('app' + pageName).set({
+
+    company: pageCompanyName,
+    email: pageEmail,
+    jobPosition : pageJobPosition,
+    jobDescription: pageJobDescription
+  });
+}
