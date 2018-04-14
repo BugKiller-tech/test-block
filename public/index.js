687828c2d49fa3e1aab6c1472b45778f602e1ce9
dcfritsc <danielcfritsche@gmail.com>
2018-04-14 12:25:34 -0500

    added url to databse input

--- public/index.js
+++ public/index.js
@@ -7,12 +7,13 @@ function writeUserData() {
   let pageEmail = document.getElementById("email").value;
   let pageJobPosition = document.getElementById("jobPosition").value;
   let pageJobDescription = document.getElementById("jobDescription").value;
-
+  let pageUrl = document.getElementById("jobUrl").value;
   firebase.database().ref('app' + pageName).set({
 
     company: pageCompanyName,
     email: pageEmail,
     jobPosition : pageJobPosition,
-    jobDescription: pageJobDescription
+    jobDescription: pageJobDescription,
+    jobUrl: pageUrl
   });
 }
