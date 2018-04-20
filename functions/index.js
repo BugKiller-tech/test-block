e5d181cc71cbdcbc0ba0707bf3e4b7a99d9c3485
hkg <hkg328@outlook.com>
2018-04-20 19:47:58 +0800

    charge function added

--- functions/index.js
+++ functions/index.js
@@ -10,15 +10,30 @@ const functions = require('firebase-functions');
 
 // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
+
 var stripe = require("stripe")("sk_test_IOqUUmheBCzexCbNcCnHmPNQ");
 
-// Token is created using Checkout or Elements!
-// Get the payment token ID submitted by the form:
-const token = request.body.stripeToken; // Using Express
 
-const charge = stripe.charges.create({
-  amount: 999,
-  currency: 'usd',
-  description: 'Example charge',
-  source: token,
+exports.charge = functions.https.onRequest((req, res) => {
+
+  
+  // Token is created using Checkout or Elements!
+  // Get the payment token ID submitted by the form:
+  
+  const token = req.body.stripeToken; // Using Express
+
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: token
+  })
+  .then(customer => stripe.charges.create({
+    amount: 999,
+    description: 'Example charge',
+    currency: 'usd',
+    customer: customer.id
+  }))
+  .then(charge => res.json({success: true, message: 'Successfully paid'}))
+  .catch(err => {
+    res.status(400).json({ errors: JSON.stringify(err) })
+  })
 });
